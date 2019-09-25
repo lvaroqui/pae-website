c2eadd5679e504b0f8ac15f5a9ec1e6884056c10
Luc Varoqui <luc@varoqui.org>
2019-09-25 15:28:30 +0200

    Updated Front Error message

--- front/layouts/error.vue
+++ front/layouts/error.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <h1 v-if="error.statusCode === 404">
       {{ pageNotFound }}
     </h1>
@@ -7,7 +7,7 @@
       {{ otherError }}
     </h1>
     <NuxtLink to="/">
-      Home page
+      Retour à l'accueil
     </NuxtLink>
   </v-app>
 </template>
@@ -30,8 +30,8 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      pageNotFound: "Oups, la page n'a pas été trouvée !",
+      otherError: "Une erreure s'est produite"
     }
   }
 }
